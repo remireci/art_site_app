3a33b064eb727b4f165cdd84660e1c676aeb1e47
remireci <reci.reciproque@gmail.com>
2025-02-03 20:44:41 +0100

    fix: env variables

--- app/lib/db.js
+++ app/lib/db.js
@@ -10,15 +10,15 @@ import mongoose from 'mongoose';
 
 let cached = global.mongoose;
 
-if(!cached) {
-    cached = global.mongoose = { conn: null};
+if (!cached) {
+    cached = global.mongoose = { conn: null };
 }
 
 
 export const dbConnect = async () => {
     if (cached.conn) return cached.conn;
 
-    cached.conn = await mongoose.connect(process.env.MONGODB_URI);
+    cached.conn = await mongoose.connect(process.env.NEXT_PUBLI_MONGODB_URI);
 
     return cached.conn;
 };
