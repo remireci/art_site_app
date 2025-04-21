308c76e4a3987f7e5ffcc1e939cbaa6a3560471c
remireci <reci.reciproque@gmail.com>
2025-04-21 23:02:37 +0200

    adding image url's to head

--- app/components/ImageDisplay.jsx
+++ app/components/ImageDisplay.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import Image from 'next/image';
+import Head from 'next/head';
 
 // Initialize Supabase client
 // const supabase = createClient(
@@ -9,7 +10,39 @@ import Image from 'next/image';
 //     process.env.NEXT_PUBLIC_SUPABASE_API_KEY
 // );
 
-const ImageDisplay = ({ imagePath, title, priority = false }) => {
+const ImageDisplay = ({ imagePath, title, priority = false, index = 0 }) => {
+
+    const [isInViewport, setIsInViewport] = useState(false);
+    // Get the full path after 'agenda/', and remove any query parameters
+    const imageName = imagePath.split('?')[0].split('agenda/')[1];
+
+    // Construct the Cloudflare optimized URL
+    const optimizedUrl = `https://img.artnowdatabase.eu/cdn-cgi/image/width=300,quality=70,fit=cover,format=auto/agenda/${encodeURI(imageName)}`;
+
+    useEffect(() => {
+        // Preload the first 2 images based on the priority
+        if (priority) {
+            const preloadLink = document.createElement('link');
+            preloadLink.rel = 'preload';
+            preloadLink.href = optimizedUrl;
+            preloadLink.as = 'image';
+            document.head.appendChild(preloadLink);
+        }
+
+        // Intersection Observer for lazy loading
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    setIsInViewport(true);
+                }
+            },
+            { threshold: 0.1 } // Trigger once 10% of the image is visible
+        );
+
+        observer.observe(document.querySelector(`#image-${index}`));
+
+        return () => observer.disconnect();
+    }, [priority, index, optimizedUrl]);
     // const [imageUrl, setImageUrl] = useState('');
 
     // console.log("from ImageDisplay", imagePath);
@@ -46,29 +79,33 @@ const ImageDisplay = ({ imagePath, title, priority = false }) => {
 
     // const optimizedUrl = `https://img.artnowdatabase.eu/cdn-cgi/image/width=300,fit=cover/agenda/${encodeURIComponent(imageName)}`;
 
-    // Get the full path after 'agenda/', and remove any query parameters
-    const imageName = imagePath.split('?')[0].split('agenda/')[1];
 
-    // Construct the Cloudflare optimized URL
-    const optimizedUrl = `https://img.artnowdatabase.eu/cdn-cgi/image/width=300,quality=70,fit=cover,format=auto/agenda/${encodeURI(imageName)}`;
 
     return (
-        <div className={`relative w-full aspect-[1]`}>
-            {/* <div className="relative h-[100px]"> */}
-            <Image
-                unoptimized
-                priority={priority}
-                src={optimizedUrl}
-                alt={title}
-                layout="fill" // This ensures the image fills the container
-                objectFit="cover" // Ensures the image fits within the dimensions
-                placeholder='blur'
-                blurDataURL='"/placeholder.png'
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Optimize based on screen size
-                className="transition-transform duration-700 ease-in-out hover:scale-[1.4] hover:z-10"
-            />
-            {/* </div> */}
-        </div>
+        <>
+            <Head>
+                {/* Preload the images with priority (first 2 images) */}
+                {priority && (
+                    <link rel="preload" href={optimizedUrl} as="image" />
+                )}
+            </Head>
+            <div id={`image-${index}`} className={`relative w-full aspect-[1]`}>
+                {/* <div className="relative h-[100px]"> */}
+                <Image
+                    unoptimized
+                    priority={priority}
+                    src={optimizedUrl}
+                    alt={title}
+                    layout="fill" // This ensures the image fills the container
+                    objectFit="cover" // Ensures the image fits within the dimensions
+                    placeholder='blur'
+                    blurDataURL='"/placeholder.png'
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Optimize based on screen size
+                    className="transition-transform duration-700 ease-in-out hover:scale-[1.4] hover:z-10"
+                />
+                {/* </div> */}
+            </div>
+        </>
     );
 };
 
