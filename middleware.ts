0295b7feb8bb7dae06c837ae7ae6bca606ebbee4
remireci <reci.reciproque@gmail.com>
2025-11-27 22:01:17 +0100

    block regions and IP - suspicious

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { locales } from "./i18n.config";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
+// import { Ratelimit } from "@upstash/ratelimit";
+// import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-});
+// const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL!,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+// });
 
-const ratelimit = new Ratelimit({
-  redis,
-  limiter: Ratelimit.slidingWindow(5, "10 s"), // 5 requests per 10 seconds per IP
-});
+// const ratelimit = new Ratelimit({
+//   redis,
+//   limiter: Ratelimit.slidingWindow(5, "10 s"), // 5 requests per 10 seconds per IP
+// });
 
 const intlMiddleware = createMiddleware({
   defaultLocale: "en",
